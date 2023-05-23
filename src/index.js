a90b1799913addee39535a221558d212e6585575
Claydson Henrique <claydsonhenriquebs@gmail.com>
2023-05-23 16:34:39 -0300

    adicionando store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  );
+ReactDOM.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,19 @@
-// configure aqui sua store
+import { createStore } from 'redux';
+
+// Reducer
+const initialState = { count: 0 };
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+  case 'INCREMENT':
+    return { count: state.count + 1 };
+  case 'DECREMENT':
+    return { count: state.count - 1 };
+  default:
+    return state;
+  }
+};
+
+const store = createStore(reducer);
+
+export default store;
